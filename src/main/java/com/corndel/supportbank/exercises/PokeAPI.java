057dc2731b0d0b61527e273cf48573e7ea27abfe
James Kett <james2kett@gmail.com>
2024-10-03 12:17:05 +0100

    exercises day 3 - APIs

--- src/main/java/com/corndel/supportbank/exercises/PokeAPI.java
+++ src/main/java/com/corndel/supportbank/exercises/PokeAPI.java
@@ -1,9 +1,9 @@
 package com.corndel.supportbank.exercises;
 
-// import kong.unirest.Unirest;
-
-// import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import kong.unirest.ObjectMapper;
+import kong.unirest.Unirest;
 
 /**
  * This class represents a Pokemon. It uses Java's record syntax to
@@ -27,15 +27,25 @@ public class PokeAPI {
    */
   public static Pokemon getPokemonByName(String name) throws Exception {
     // TODO: Create the url by appending the name to the base url
-
+    String url = "https://pokeapi.co/api/v2/pokemon/" + name;
     // TODO: Make a GET request to the url
     // Hint: Use Unirest.get()
+    var response = Unirest
+            .get(url)
+            .header("Accept","application/json")
+            .asString();
+
+    String json = response.getBody();
+    //System.out.println(json);
 
     // TODO: Parse the response body into a Pokemon object
     // Hint: Use Jackson's ObjectMapper to map the response body to Pokemon.class
+    ObjectMapper mapper = new ObjectMapper();
+    Pokemon pokemon = mapper.readValue(json, Pokemon.class);
+    System.out.println(pokemon);
 
     // TODO: Return the Pokemon
-    return null;
+    return pokemon;
   }
 
   /**
--- src/main/java/com/corndel/supportbank/exercises/Postman.java
+++ src/main/java/com/corndel/supportbank/exercises/Postman.java
@@ -2,6 +2,8 @@ package com.corndel.supportbank.exercises;
 
 // import kong.unirest.Unirest;
 
+import kong.unirest.Unirest;
+
 /**
  * This class represents a Message to be sent to the Postman Echo API.
  * You don't need to modify it.
@@ -28,12 +30,19 @@ public class Postman {
    */
   public static String echoMessage(int id, String content) {
     // TODO: Create a Message object with the given id and content
+    Message message = new Message(id, content);
+    String url = "https://postman-echo.com/post";
 
     // TODO: Post the Message object to the Postman Echo API
     // Hint: Use Unirest.post()
-
+    var response = Unirest.post(url)
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .body(message) // convert to json and send to API
+            .asString(); // get response as a string
     // TODO: Return the response body as a string of JSON
-    return null;
+    String json = response.getBody(); // convert into a string of json
+    return json;
   }
 
   /**
